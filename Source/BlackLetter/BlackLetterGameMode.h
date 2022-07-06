6d8a3ebe46d259f5d50bca107cc009ec3f8f3938
Rifat Ahammed <reardj007@gmail.com>
2022-07-06 18:07:04 +0600

    Init Project

---
+++ Source/BlackLetter/BlackLetterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlackLetterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABlackLetterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABlackLetterGameMode();
+};
+
+
+
